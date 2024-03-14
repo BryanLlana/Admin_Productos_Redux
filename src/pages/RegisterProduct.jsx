f7095091762b8652ae018ac69c5ff92515eae875
Bryan Joseph <bryanllana14@gmail.com>
2024-03-14 12:05:19 -0500

    Design register product

--- src/pages/RegisterProduct.jsx
+++ src/pages/RegisterProduct.jsx
@@ -1,7 +1,34 @@
 const RegisterProduct = () => {
   return (
     <>
-      <h1>Register</h1>
+      <div className="row justify-content-center">
+        <div className="col-md-8">
+          <div className="card">
+            <div className="card-body">
+              <h2 className="text-center mb-4 font-weight-bold">
+                Agregar Nuevo Producto
+              </h2>
+
+              <form>
+                <div className="form-group">
+                  <label htmlFor="name">Nombre</label>
+                  <input type="text" className="form-control" id="name" name="name" />
+                </div>
+                <div className="form-group">
+                  <label htmlFor="price">Precio</label>
+                  <input type="text" className="form-control" id="price" name="price" />
+                </div>
+
+                <button
+                  type="submit"
+                  className="btn btn-primary font-weight-bold text-uppercase d-block w-100"
+                >Agregar Producto
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
